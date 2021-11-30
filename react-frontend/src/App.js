251814a99d70b21352ae7e6da1adc62571da1738
Khandaker Shafiqul Islam <ksishafiq007@gmail.com>
2021-11-30 23:58:39 +0600

    React router dom added
    React router dom added

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,12 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import ListEmployeeComponent from './components/ListEmployeeComponent';
+import HeaderComponent from './components/HeaderComponent';
+import FooterComponent from './components/FooterComponent';
 
 function App() {
   return (
-    <div className="container">
-      <ListEmployeeComponent />
+    <div>
+      <Router>
+        <HeaderComponent />
+          <div className="container">
+            <Switch>
+              <Route path="/" component={ListEmployeeComponent}></Route>
+              <Route path="/employees" component={ListEmployeeComponent}></Route>
+              <ListEmployeeComponent />
+            </Switch>           
+          </div>
+        <FooterComponent />
+      </Router>
+      
     </div>
+    
+    
   );
 }
 
